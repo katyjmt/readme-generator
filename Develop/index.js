70c6e02e6163d5dd226ffad30edda21d943ee4f5
katyjmt <43462706+katyjmt@users.noreply.github.com>
2023-07-24 12:15:30 +1000

    Installed inquirer, fs and added user questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,72 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address? (So people can reach you if they have questions about your application.)',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:\n- What was your motivation?\nWhy did you build this project?\nWhat problem does it solve?\nWhat did you learn?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Provide instructions and examples for use.',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Please provide the file path to a screenshot or video of your project.',
+        name: 'image',
+    },
+    {
+        type: 'input',
+        message: 'List your collaborators, if any, with links to their GitHub profiles.\nIf you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.\nIf you followed tutorials, include links to those here as well.',
+        name: 'credits',
+    },
+    {
+        type: 'list',
+        message: 'Please select the license used for your project.',
+        choices: ['MIT License', 'GNU GPLv3', 'GNU AGPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you\'d prefer.',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'If relevant, provide tests for your application and examples of how to run them.',
+        name: 'tests',
+    },
+];
+
+inquirer
+    .prompt(questions)
+    .then((answers) => {
+        // What to do with answers
+    })
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
